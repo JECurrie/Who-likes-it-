0980f4330069eb88ee64e683595545823c16bdce
Joan Currie <jecurrie8@gmail.com>
2020-01-09 22:40:18 -0500

    Initial commit

--- likes.rb
+++ likes.rb
@@ -1,8 +1,8 @@
 def likes(names)
 	len = names.length
-	"no one likes this" if names == []
-  "#{names[0]} like this" if len == 1
-  "#{names[0]} and #{names[1]}  like this" if len == 2
-  "#{names[0]}, #{names[1]} and #{names[2]} like this" if len == 3
+	return "no one likes this" if names == []
+  return "#{names[0]} likes this" if len == 1
+  return "#{names[0]} and #{names[1]} like this" if len == 2
+  return "#{names[0]}, #{names[1]} and #{names[2]} like this" if len == 3
   "#{names[0]}, #{names[1]} and #{len - 2} others like this" if len > 3
 end
